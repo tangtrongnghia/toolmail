cd71b6354166a67a23b2c2ac5aedec4fa74a3bdc
Tang Trong Nghia <nghiaapag@gamil.com>
2025-02-21 00:06:24 +0700

    update bat file

--- serve.ps1
+++ serve.ps1
@@ -1,11 +1,33 @@
-# Lấy địa chỉ IP từ ipconfig
-$ipconfig = ipconfig
-$ipv4Address = $ipconfig | Select-String -Pattern "IPv4 Address" | ForEach-Object {
-    $_ -replace ".*:\s*"
+# Lấy phiên bản Windows
+$osVersion = (Get-CimInstance Win32_OperatingSystem).Version
+
+# Hàm lấy địa chỉ IPv4
+function Get-IPv4Address {
+    param ($interfaceAlias)
+    return (Get-NetIPAddress -AddressFamily IPv4 -InterfaceAlias $interfaceAlias | Where-Object { $_.AddressState -eq "Preferred" }).IPAddress
 }
 
-# Lấy dòng đầu tiên vì có thể có nhiều kết quả
-$ipv4Address = $ipv4Address[0]
+# Kiểm tra phiên bản Windows
+if ($osVersion -ge "10.0") {
+    # Windows 10 hoặc cao hơn
+    $ipv4Address = Get-IPv4Address -interfaceAlias "Wi-Fi"
+    if (-not $ipv4Address) {
+        $ipv4Address = Get-IPv4Address -interfaceAlias "Ethernet"
+    }
+} else {
+    # Windows phiên bản thấp hơn
+    $ipconfig = ipconfig
+    $ipv4Address = $ipconfig | Select-String -Pattern "IPv4 Address. . . . . . . . . . . :" | ForEach-Object {
+        $_ -replace ".*:\s*"
+    }
+    $ipv4Address = $ipv4Address[0]
+}
+
+# Kiểm tra xem đã lấy được địa chỉ IP hay chưa
+if (-not $ipv4Address) {
+    Write-Output "Không tìm thấy địa chỉ IP hợp lệ."
+    exit 1
+}
 
 # Chạy lệnh PHP Artisan Serve
 $port = 80
